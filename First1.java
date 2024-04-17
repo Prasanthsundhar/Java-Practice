c7c271691d720e4022d663853d7d074e5378867e
prasanth <prasanth@inflexiontechfze.com>
2024-04-17 11:31:01 +0530

    commit the first1.java

---
+++ First1.java
@@ -0,0 +1,11 @@
+public class First1 {
+    /*my first java programe */
+      //String a = "Prasanth Good Morning!";
+    public static void main(String[] args) {
+        int number = 50;
+
+        First1 msg = new First1();
+        System.out.println(number);
+        
+    }
+}
